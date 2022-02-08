44e764a8771f1a94e1f7c7ff93893378f8dda024
WRTmMRT <16154@my.sanctamaria.school.nz>
2022-02-09 11:00:00 +1300

    Update main_app.py
    Started website

--- main_app.py
+++ main_app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from numpy import true_divide
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<h1>Hello, World!</h1>"
+
+
+if __name__ == '__main__' :
+    app.run(debug=True)
