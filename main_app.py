ffb65edc76c08ebbc648c64c13e9a82a47f6e3d8
WRTmMRT <16154@my.sanctamaria.school.nz>
2022-03-07 21:28:45 +1300

    Created package
    Changed the file setup so that the program runs as a package

--- main_app.py
+++ main_app.py
@@ -1,7 +1,37 @@
-from flask import flash, redirect, render_template, url_for
-from main_app import app
-from main_app.forms import RegistrationForm, LoginForm
-from main_app.models import User, Post
+from pydoc import render_doc
+from flask import Flask, flash, redirect, render_template, url_for
+from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '16f147ed7bc9d2e0b8af5e0f59ba0ca1'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+    
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}','{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    
+    def __repr__(self):
+        return f"Post('{self.title}','{self.date_posted}')"
+
+
 
 posts = [
     
@@ -57,6 +87,8 @@ def login():
         if form.email.data == "admin@post.com" and form.password.data == "password":
             flash('You have been logged in!', 'success')
             return redirect(url_for('home'))
-    else:
-        flash('Login unsuccessful, Please check your username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
+
+#Allows main app to run the web server
+if __name__ == '__main__' :
+    app.run(debug=True)
