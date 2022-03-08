7141be4e7bb88b6a86df82e4c54ffca5f092cd37
WRTmMRT <16154@my.sanctamaria.school.nz>
2022-03-09 10:25:58 +1300

    added Validation
    added validation for username and email so that they are unique when registering

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,6 +1,8 @@
+from xml.dom import ValidationErr
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from main_app.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
@@ -9,7 +11,18 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
     
-    
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose another one')
+        
+    def validate_email(self, email):
+        email_check = User.query.filter_by(email=email.data).first()
+        if email_check:
+            raise ValidationError('That email is taken. Please choose another one')
+        
+        
+        
     
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
