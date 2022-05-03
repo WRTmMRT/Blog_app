b0622fca6890ebaac2e5eb9bfa4b8d0f1a201b7e
WRTmMRT <16154@my.sanctamaria.school.nz>
2022-05-04 11:00:00 +1200

    Created Updated Account forms

--- main_app/routes.py
+++ main_app/routes.py
@@ -1,6 +1,6 @@
 from flask import flash, redirect, render_template, url_for, request
 from main_app import app, db, bcrypt
-from main_app.forms import RegistrationForm, LoginForm
+from main_app.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from main_app.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -79,7 +79,18 @@ def logout():
     return redirect(url_for('home'))
 
 
-@app.route("/account")
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated','success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
