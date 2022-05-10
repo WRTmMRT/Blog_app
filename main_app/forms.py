7ba9456424d49b40df5203fb94822117706d3e08
WRTmMRT <16154@my.sanctamaria.school.nz>
2022-05-10 15:09:33 +1200

    Resized thumbnails and created post writer

--- main_app/forms.py
+++ main_app/forms.py
@@ -2,7 +2,7 @@ from xml.dom import ValidationErr
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from main_app.models import User
 
@@ -50,3 +50,7 @@ class UpdateAccountForm(FlaskForm):
             if email_check:
                 raise ValidationError('That email is taken. Please choose another one')
             
+class PostForm(FlaskForm):
+     title = StringField('Title', validators=[DataRequired()])
+     content = TextAreaField('Content', validators=[DataRequired()])
+     submit = SubmitField('Post')
--- main_app/routes.py
+++ main_app/routes.py
@@ -1,8 +1,9 @@
 import os
 import secrets
+from PIL import Image
 from flask import flash, redirect, render_template, url_for, request
 from main_app import app, db, bcrypt
-from main_app.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from main_app.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from main_app.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -88,8 +89,11 @@ def save_picture(form_picture):
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
-    form_picture.save(picture_path)
     
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
     return picture_fn
 
 @app.route("/account", methods=['GET', 'POST'])
@@ -110,3 +114,17 @@ def account():
         form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account', image_file=image_file, form=form)
+
+
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post',
+                           form=form, legend='New Post')
