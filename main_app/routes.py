e7cc29788d47baf68370c9deb396d42418fcb5f5
WRTmMRT <16154@my.sanctamaria.school.nz>
2022-05-17 13:13:55 +1200

    added update post
    added the ability to update posts

--- main_app/routes.py
+++ main_app/routes.py
@@ -116,11 +116,20 @@ def post(post_id):
 
 
 # Route for update/delete posts
-@app.route("/post/<int:post_id>/update")
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
 @login_required
 def update_post(post_id):
     post = Post.query.get_or_404(post_id)
     if post.author != current_user:
         abort(403)
     form = PostForm()
-    return render_template('create_post.html', title='Update Post', form=form, legend='New Post')
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('post', post_id=post_id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post', form=form, legend='Update Post')
